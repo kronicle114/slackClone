1e384c851e83c6784b62c7210d9bf1b96aa48027
Patrick O'Connor <poc.967@gmail.com>
2021-09-12 22:15:39 -0400

    add all CRUD functions for a company

--- routes/company.js
+++ routes/company.js
@@ -1,8 +1,123 @@
 const express = require("express");
 const companyRouter = express.Router();
+const { Company } = require('../models/company');
+const { User } = require('../models/user');
 
-companyRouter.get("/", (request, response, next) => {
-  return response.status(200).json("hello, world!");
+// GET all companies
+companyRouter.get('/', async (request, response, next) => {
+  console.log('GET company by ID');
+  let companies = await Company.find({ is_deleted: false });
+
+  if (!companies) {
+    return response.status(400).json({ message: 'no companies found' });
+  }
+
+  companies = companies.map((company) => {
+    return company.serialize();
+  });
+
+  return response.status(200).json(companies);
+});
+
+// Get company by ID
+companyRouter.get('/:id', async (request, response, next) => {
+  console.log('GET all companies');
+  const { id } = request.params;
+
+  let company = await Company.findOne({ _id: id });
+
+  if (!company) {
+    return response.status(400).json({ message: 'no company found' });
+  }
+
+  return response.status(200).json(company.serialize());
+});
+
+// POST create company
+companyRouter.post('/', async (request, response, next) => {
+  console.log('POST create company');
+  let { user, name } = request.body;
+
+  // check for missing required parameters
+  let requiredFields = ['user', 'name'];
+  let missingFields = [];
+  requiredFields.forEach((field) => {
+    if (!request.body[field] && field !== '') {
+      missingFields.push(field);
+    }
+  });
+
+  if (missingFields.length > 0) {
+    return response
+      .status(400)
+      .json({ message: `required field(s) missing: ${missingFields}` });
+  }
+
+  // ensure the user provided is indeed a real user
+  let userExists = await User.findOne({ _id: user });
+
+  if (!userExists) {
+    return response.status(400).json({
+      message: 'user does not exist. Please provide a valid admin user',
+    });
+  }
+
+  // create the new company object
+  let company = await new Company({
+    admin: user,
+    name,
+  });
+
+  if (!company) {
+    return response
+      .status(400)
+      .json({ message: 'the company could not be created' });
+  }
+  company.save();
+
+  return response.status(201).json({ message: 'company created' });
+});
+
+// PUT company
+companyRouter.put('/:id', async (request, response, next) => {
+  console.log('PUT edit company');
+  const { id } = request.params;
+
+  let editableFields = ['name'];
+
+  let company = await Company.findOne({ _id: id });
+
+  if (!company) {
+    return response.status(400).json({ message: 'company could not be found' });
+  }
+
+  editableFields.forEach((field) => {
+    if (request.body[field] && company[field]) {
+      company[field] = request.body[field];
+    }
+  });
+
+  company.save();
+
+  return response.status(200).json(company);
+});
+
+// DELETE company
+companyRouter.delete('/:id', async (request, response, next) => {
+  console.log('DELETE company');
+  const { id } = request.params;
+
+  let company = await Company.findOne({ _id: id });
+
+  if (!company) {
+    return response.status(400).json({ message: 'company does not exist' });
+  } else {
+    company.is_deleted = true;
+    company.save();
+    return response
+      .status(200)
+      .json({ message: 'company deleted successfully' });
+  }
 });
 
 module.exports = companyRouter;
